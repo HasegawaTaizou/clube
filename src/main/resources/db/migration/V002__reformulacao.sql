f0d1c2fbffc59a6727a6d87d99dd8ef8469adea0
apenas1ignorante <58717553+apenas1ignorante@users.noreply.github.com>
2021-06-01 20:31:23 -0300

    Update V002__reformulacao.sql

--- src/main/resources/db/migration/V002__reformulacao.sql
+++ src/main/resources/db/migration/V002__reformulacao.sql
@@ -26,10 +26,10 @@ create table usuario (
   endereco_bairro varchar(255) not null,
   endereco_cep varchar(255) not null,
   endereco_logradouro varchar(255) not null,
-  favoritos int null,
+  favorito int null,
   endereco_cidade_id bigint not null,
-  anuncio_id bigint not null,
-  compra_id bigint not null,
+  anuncio_id bigint null,
+  compra_id bigint null,
 
   primary key (id, endereco_cidade_id, anuncio_id, compra_id)
 ) engine=InnoDB default charset=utf8;
@@ -52,9 +52,10 @@ create table compra (
   id bigint not null auto_increment,
   data_compra datetime(6) not null,
   quantidade integer not null,
+  (chave estrangeira em anuncio ou compra?)
   
   primary key (id)
-)
+) engine=InnoDB default charset=utf8;
 
 alter table cidade add constraint fk_cliente_estado 
 foreign key (estado_id) references estado (id);
@@ -75,3 +76,11 @@ insert into estado (id, nome) values (3, 'Ceará');
 insert into cidade (id, nome, estado_id) values (1, 'Uberlândia', 1);
 insert into cidade (id, nome, estado_id) values (2, 'São Paulo', 2);
 insert into cidade (id, nome, estado_id) values (3, 'Fortaleza', 3);
+
+insert into usuario (id, data_cadastro, ativo, nome, sobrenome, senha, email, sexo, endereco_pais, endereco_bairro, endereco_cep, endereco_logradouro, favorito, endereco_cidade_id, anuncio_id, compra_id) values (1, utc_timestamp, 1, 'joao', 'pereira', 'abc', 'joaopereira@gmail.com', 'M', 'Brasil', 'aaaa', '11111-111', 'aaaa', 0, 'aaaa', null, null);
+insert into usuario (id, data_cadastro, ativo, nome, sobrenome, senha, email, sexo, endereco_pais, endereco_bairro, endereco_cep, endereco_logradouro, favorito, endereco_cidade_id, anuncio_id, compra_id) values (2, utc_timestamp, 1, 'guilherme', 'oliveira', 'def', 'guilhermeoliveira@gmail.com', 'M', 'Brasil', 'bbbb', '2222-222', 'bbbb', 0, 'bbbb', null, 1);
+insert into usuario (id, data_cadastro, ativo, nome, sobrenome, senha, email, sexo, endereco_pais, endereco_bairro, endereco_cep, endereco_logradouro, favorito, endereco_cidade_id, anuncio_id, compra_id) values (3, utc_timestamp, 1, 'gabriel', 'silva', 'ghi', 'gabrielsilva@gmail.com', 'M', 'Brasil', 'cccc', '33333-333', 'cccc', 0, 'cccc', 1, null);
+
+insert into anuncio () values (); anuncio pode ser comprado N vezes e a compra pode ter N anuncio
+
+insert into compra (id, data_compra, quantidade, anuncio_id) values (1, '2020-11-15 13:14:30', 1, 1);
